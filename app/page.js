d86372877f632c1d77a70cba91011a75f63c13d9
danwaelt <danwaelt@gmail.com>
2025-06-25 14:38:20 +1000

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ fontFamily: "sans-serif", textAlign: "center", padding: "4rem" }}>
+      <h1>Welcome to Connective Shift!</h1>
+      <p>Your website is now live on Vercel using Next.js.</p>
+      <p>Edit <code>app/page.js</code> to get started building your site.</p>
+    </main>
+  );
+}
